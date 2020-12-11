e9c631da67d0dcd5c0bef9544419eb9d1db9c9b1
ericcalasans <ericcalasans@gmail.com>
2020-12-11 11:35:03 -0300

    Reestruturação

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "myclientudp.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    system("clear");
+    printf("***\tPROGRAMA CLIENTE\t***\n");
+
+    try {
+        myclientudp cliente_udp(PORTA);
+        cliente_udp.echo();
+
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << "\n";
+    }
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,74 +1,29 @@
 //
 // Created by Eric Calasans on 11/12/20.
 //
-// Server side implementation of UDP client-server model
-#include <stdio.h>
-#include <stdlib.h>
+
+#define PORTA 4140
+#define MAX_SIZE 0x10
+
+#include <array>
+#include <iostream>
+#include <stdexcept>
+
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
-#include <iostream>
+#include "myserverudp.h"
 
-#define PORT	 4140
-#define MAXLINE 1024
-
-// Driver code
-int main() {
-    int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from server";
-    struct sockaddr_in servaddr, cliaddr;
-    int velocidades[4];
-
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
+int main(){
+    system("clear");
+    try{
+        myserverudp servidor(PORTA);
+        servidor.echo();
     }
-
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
-
-    // Filling server information
-    servaddr.sin_family = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(PORT);
-
-    // Bind the socket with the server address
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr,
-              sizeof(servaddr)) < 0 )
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    catch(const std::exception& e){
+        std::cerr << e.what() << '\n';
     }
-
-    socklen_t len, n;
-
-    len = sizeof(cliaddr); //len is value/resuslt
-
-    recvfrom(sockfd, velocidades, sizeof(velocidades),
-             MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
-    if (len > 0){
-        std::cout << cliaddr.sin_addr.s_addr << std::endl;
-        for (int i = 0; i < 4; i++)
-        {
-            printf("Velocidade %d recebida:  %d\n", i, velocidades[i]);
-        }
-    } else {
-        printf("Não veio nada!\n");
-    }
-
-    sendto(sockfd, (const char *)hello, strlen(hello),
-           0, (const struct sockaddr *) &cliaddr,
-           len);
-    printf("Hello message sent.\n");
-
     return 0;
 }
-
-
-
 
